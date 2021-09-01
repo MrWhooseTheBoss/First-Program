5a0dfdcc0cb6b25700365db33506807377abf0f8
MrWhooseTheBoss <89867823+MrWhooseTheBoss@users.noreply.github.com>
2021-09-01 14:20:54 -0400

    Create First Program.cs

---
+++ First Program.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int num01; 
+            int num02;
+            
+            Console.Write("Input a number: ");
+            num01 =Convert.ToInt32(  Console.ReadLine() );
+
+            Console.Write("Input a second number: ");
+            num02 =Convert.ToInt32(Console.ReadLine() );
+
+            int result = num01 * num02;
+
+            Console.WriteLine("The result is " + result);
+            
+        // Wait before closeing 
+            Console.ReadKey();
+            
+        }
+    }
+}
